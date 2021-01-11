8d7f2949d8135b20745adcf6e4df9913b309b858
Oleg Korotin <o9818154818@yandex.ru>
2021-01-11 17:46:17 +0300

    Update AlarmBroadcastReceiver.java

--- packages/android_alarm_manager/android/src/main/java/io/flutter/plugins/androidalarmmanager/AlarmBroadcastReceiver.java
+++ packages/android_alarm_manager/android/src/main/java/io/flutter/plugins/androidalarmmanager/AlarmBroadcastReceiver.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.PowerManager;
 import android.content.pm.PackageManager;
-import android.view.WindowManager.LayoutParams;
 
 public class AlarmBroadcastReceiver extends BroadcastReceiver {
   private static PowerManager.WakeLock wakeLock;
@@ -14,7 +13,7 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver {
   public void onReceive(Context context, Intent intent) {
     PowerManager powerManager = (PowerManager)
 context.getSystemService(Context.POWER_SERVICE);
-    wakeLock = powerManager.newWakeLock(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+    wakeLock = powerManager.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK |
             PowerManager.ACQUIRE_CAUSES_WAKEUP |
             PowerManager.ON_AFTER_RELEASE, "My wakelock");
 
